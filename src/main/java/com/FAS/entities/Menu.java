5a845e8a60fae9e7ef6d0252d53afefc335b1011
Luis Kcomt <45114652+kcomt@users.noreply.github.com>
2019-05-14 14:38:09 -0500

    Algunas clases implementadas
    SaleReport, Menu

--- src/main/java/com/FAS/entities/Menu.java
+++ src/main/java/com/FAS/entities/Menu.java
@@ -1,5 +1,38 @@
 package com.FAS.entities;
 
-public class Menu {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
 
+@Entity
+@Table(name = "menu")
+public class Menu {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Size(min = 10, message = "Description must be atleast 10 charecters in length")
+	@Column(name = "description", nullable = false, length = 150)
+	private String description;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
--- src/main/java/com/FAS/entities/SaleReport.java
+++ src/main/java/com/FAS/entities/SaleReport.java
@@ -1,5 +1,49 @@
 package com.FAS.entities;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sale_report")
 public class SaleReport {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "total", nullable = false)
+	private float total;
+	
+	@Column(name="date")
+	private LocalDate date;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public float getTotal() {
+		return total;
+	}
+
+	public void setTotal(float total) {
+		this.total = total;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	} 
 }
