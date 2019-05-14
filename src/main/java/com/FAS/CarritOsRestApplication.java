9b694253ad050611a60b9bb3b475a9ab3b3c8d51
Luis Kcomt <45114652+kcomt@users.noreply.github.com>
2019-05-14 13:13:18 -0500

    Proyecto Creado
    usando spring boot

---
+++ src/main/java/com/FAS/CarritOsRestApplication.java
@@ -0,0 +1,13 @@
+package com.FAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarritOsRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarritOsRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FAS/CarritOsRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.FAS;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarritOsRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
