df4ba042de5e8ebb069afbd832030f7cd329e615
DiegoAloslla <diegoalosilla@gmail.com>
2019-05-24 00:11:27 -0500

    impl service

--- src/main/java/com/FAS/entities/Contract.java
+++ src/main/java/com/FAS/entities/Contract.java
@@ -1,7 +1,4 @@
 package com.FAS.entities;
-
-
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
---
+++ src/main/java/com/FAS/service/impl/BuisnessOwnerServiceImpl.java
@@ -0,0 +1,62 @@
+package com.FAS.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.FAS.entities.BuisnessOwner;
+import com.FAS.repository.BuisnessOwnerRepository;
+import com.FAS.service.IBuisnessOwnerService;
+
+@Service
+public class BuisnessOwnerServiceImpl implements IBuisnessOwnerService{
+
+	@Autowired
+	private BuisnessOwnerRepository buisnessOwnerRepository;
+	
+	@Transactional(readOnly=true)
+	@Override
+	public List<BuisnessOwner> findAll() throws Exception {
+		// TODO Auto-generated method stub
+		return buisnessOwnerRepository.findAll();
+	}
+
+	@Transactional
+	@Override
+	public BuisnessOwner save(BuisnessOwner t) throws Exception {
+		// TODO Auto-generated method stub
+		return buisnessOwnerRepository.save(t);
+	}
+
+	@Transactional
+	@Override
+	public BuisnessOwner update(BuisnessOwner t) throws Exception {
+		// TODO Auto-generated method stub
+		return buisnessOwnerRepository.save(t);
+	}
+
+	@Transactional(readOnly=true)
+	@Override
+	public Optional<BuisnessOwner> findById(Integer id) throws Exception {
+		// TODO Auto-generated method stub
+		return buisnessOwnerRepository.findById(id);
+	}
+
+	@Transactional
+	@Override
+	public void deleteById(Integer id) throws Exception {
+		// TODO Auto-generated method stub
+		buisnessOwnerRepository.deleteById(id);
+	}
+
+	@Transactional
+	@Override
+	public void deleteAll() throws Exception {
+		// TODO Auto-generated method stub
+		buisnessOwnerRepository.deleteAll();
+	}
+	
+}
