c32cb4e436f0b288a927a96c79d22cd29dafda65
Luis Kcomt <45114652+kcomt@users.noreply.github.com>
2019-06-30 20:28:46 -0500

    ,

--- src/main/java/com/FAS/controller/FoodTruckController.java
+++ src/main/java/com/FAS/controller/FoodTruckController.java
@@ -30,7 +30,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
-@RequestMapping("/foodTrucks2")
+@RequestMapping("/foodTrucks")
 @Api(value="REST foodTruck")
 public class FoodTruckController {
 
