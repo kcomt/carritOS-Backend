bc69d53dc5f85f61f1ff5b9f02bdc511f3e589ba
Luis Kcomt <45114652+kcomt@users.noreply.github.com>
2019-07-07 17:42:31 -0500

    arregle un poco la authen

--- src/main/java/com/FAS/config/WebSecurityConfig.java
+++ src/main/java/com/FAS/config/WebSecurityConfig.java
@@ -54,7 +54,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 // We don't need CSRF for this example
 		httpSecurity.csrf().disable()
 // dont authenticate this particular request
-				.authorizeRequests().antMatchers("/authenticate", "/foodTrucks*", "/reviews*", "/consumers*").permitAll().
+				.authorizeRequests().antMatchers("/authenticate", "/foodTrucks/*", "/foodTrucks*","/reviews*", "/reviews/*", "/consumers*").permitAll().
 // all other requests need to be authenticated
 				anyRequest().authenticated().and().
 // make sure we use stateless session; session won't be used to
